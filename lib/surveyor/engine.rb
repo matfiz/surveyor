1ac24900cf1eaf807d5bf97e6b025f629a0f6123
Grzegorz Brzezinka <info@matfiz.com.pl>
2014-11-05 00:25:09 +0100

    fixes assets autoload

--- lib/surveyor/engine.rb
+++ lib/surveyor/engine.rb
@@ -8,9 +8,9 @@ module Surveyor
     config.autoload_paths << root
     initializer :assets do |config|
       Rails.application.config.assets.precompile += %w(surveyor_all.js surveyor_all.css)
-      Rails.application.config.assets.paths << root.join("lib", "assets", "images")
-      Rails.application.config.assets.paths << root.join("lib", "assets", "stylesheets")
-      Rails.application.config.assets.paths << root.join("lib", "assets", "javascripts")
+      Rails.application.config.assets.paths << "#{root}/lib/assets/images"
+      Rails.application.config.assets.paths << "#{root}/lib/assets/stylesheets"
+      Rails.application.config.assets.paths << "#{root}/lib/assets/javascripts"
     end
     config.to_prepare do
       Dir.glob(File.expand_path('../../../app/inputs/*_input*.rb', __FILE__)).each do |c|
