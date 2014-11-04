8c0bbcd14d7f975a80b1797b8d76c2aa8c5e17d1
Grzegorz Brzezinka <info@matfiz.com.pl>
2014-11-05 00:12:58 +0100

    Add assets to precompile and assets path (Rails4)

--- lib/surveyor/engine.rb
+++ lib/surveyor/engine.rb
@@ -6,6 +6,12 @@ module Surveyor
   class Engine < Rails::Engine
     root = File.expand_path('../../', __FILE__)
     config.autoload_paths << root
+    initializer :assets do |config|
+      Rails.application.config.assets.precompile += %w(surveyor_all.js surveyor_all.css)
+      Rails.application.config.assets.paths << root.join("lib", "assets", "images")
+      Rails.application.config.assets.paths << root.join("lib", "assets", "stylesheets")
+      Rails.application.config.assets.paths << root.join("lib", "assets", "javascripts")
+    end
     config.to_prepare do
       Dir.glob(File.expand_path('../../../app/inputs/*_input*.rb', __FILE__)).each do |c|
         require_dependency(c)
